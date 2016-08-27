60437f2526e9b99354e2379fcd52b7ce0a902601
Fernando Oliveira <fe10tads@live.com>
2016-08-27 16:02:10 -0300

    Primeiro Commit - Curso de Entity Framework 7

---
+++ testeNuget2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testeNuget2.Entidades;
+
+namespace testeNuget2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntidadesContext contexto = new EntidadesContext();
+            Usuario renan = new Usuario()
+            {
+                Nome = "Renan",
+                Senha = "123"
+            };
+            contexto.Usuarios.Add(renan);
+            contexto.SaveChanges();
+            contexto.Dispose();
+
+            Console.WriteLine("Salvou o usuário");
+            Console.ReadLine();
+        }
+    }
+}
